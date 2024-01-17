a9055c011ab074b615b8634caa6c4214b2455038
hex-aragon <0xaragon90@gmail.com>
2024-01-18 01:42:27 +0900

    first commit

---
+++ deposit.sh
@@ -0,0 +1,5 @@
+leo run deposit "{
+  owner: aleo address .private,
+  amount: 100u64.private,
+  _nonce: noncegroup.public
+}" 30u64
---
+++ issue.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+leo run issue "aleo address" 100u64 
+#> issue.txt
